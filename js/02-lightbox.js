66510af0d659ffae939b9d4c3d9757333a9411a2
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-09 17:08:23 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const ulElem = document.querySelector('.gallery');
+const markupGalleryItems = createMarkupGallery(galleryItems);
+ulElem.insertAdjacentHTML('beforeend', markupGalleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a',{captionsData: "alt", captionDelay: 250});
+
+
+function createMarkupGallery(galleryItems) {
+    const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;  
+              
+    });
+    return (markup.join(''));
+}
+// Закриття модалки при натисненні Esc (публічні методи бібліотеки)
+// document.addEventListener('keydown', onCloseGallery);
+// function onCloseGallery(event) {
+//     if (event.code !== "Escape") {
+//         return;
+//     }
+//     lightbox.close();
+//     console.log(event.code);
+// }
